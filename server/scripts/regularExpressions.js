10786781f36c3603b171777eaa1d7804449ac9ca
CharlieSchuyler <charlie.schuyler@outlook.com>
2023-06-19 20:24:25 +1000

    got regex working

--- server/scripts/regularExpressions.js
+++ server/scripts/regularExpressions.js
@@ -1,9 +1,11 @@
+// this page is very hard to read!
+
 const reBodyData = (data) => {
 	const reKey = "(?<=Credit.+Port.+Code).*?(?=Available Date/Time this|PLN Total Credit Hours)";
 	const templist = [];
 	for (x in data) {
 		const re = JSON.stringify(data[x].text).match(reKey);
-		templist.push({ captured: re, Individual: captureIndividualShifts(re) });
+		templist.push({ captured: re[0], Individual: captureIndividualShifts(re[0]) });
 	}
 
 	return templist;
@@ -18,23 +20,63 @@ module.exports = { reBodyData, captureIndividualShifts };
 
 //regex of individual dutuys
 
-const parseDutys = (data) => {
-	const rawRosterData = data;
-	const prevIsOvernight = false;
-	data = { isRostered: [], isNotRostered: [], isOvernight: [] };
+const parseDutys = (rawRosterData) => {
+	data = { isRostered: [], isNotRostered: [] };
 
-	//
-	if (rawRosterData[0].match(keys.isNotRostered)) {
-		currentDuty = rawRosterData[0].match(keys.isNotRostered);
-		rawRosterData[0].replace(keys.isNotRostered);
+	let end = false;
+	while (rawRosterData && !end) {
+		if (rawRosterData.match(keys.isNotRostered)) {
+			currentDuty = rawRosterData.match(keys.isNotRostered);
+			rawRosterData = rawRosterData.replace(currentDuty[0], "");
 
-		let date = currentDuty[1].split("/");
+			let date = currentDuty[1].split("/");
 
-		let organisedDuty = { date: { day: date[0], month: date[1] }, service: currentDuty[1], code: currentDuty[3], full: currentDuty[0] };
+			let organisedDuty = { date: { day: date[0], month: date[1] }, role: currentDuty[2], code: currentDuty[3], full: currentDuty[0] };
 
-		data["isNotRostered"].push(organisedDuty);
+			data["isNotRostered"].push(organisedDuty);
+		} else if (rawRosterData.match(keys.isRostered.main)) {
+			currentDuty = rawRosterData.match(keys.isRostered.main);
+			rawRosterData = rawRosterData.replace(currentDuty[0], "");
+
+			let date = currentDuty[1].split("/");
+
+			let organisedDuty = { date: { day: date[0], month: date[1] }, role: currentDuty[2], code: currentDuty[3], full: currentDuty[0] };
+
+			data["isRostered"].push(organisedDuty);
+		} else if (rawRosterData.match(keys.isRostered.isOvernight)) {
+			currentDuty = rawRosterData.match(keys.isRostered.isOvernight);
+			rawRosterData = rawRosterData.replace(currentDuty[0], "");
+
+			let date = currentDuty[1].split("/");
+
+			let organisedDuty = { date: { day: date[0], month: date[1] }, role: currentDuty[2], code: currentDuty[3], full: currentDuty[0] };
+
+			data["isRostered"].push(organisedDuty);
+		} else if (rawRosterData.match(keys.isRostered.hra_span)) {
+			currentDuty = rawRosterData.match(keys.isRostered.hra_span);
+			rawRosterData = rawRosterData.replace(currentDuty[0], "");
+
+			let date = currentDuty[1].split("/");
+
+			let organisedDuty = { date: { day: date[0], month: date[1] }, role: currentDuty[2], code: currentDuty[3], full: currentDuty[0] };
+
+			data["isRostered"].push(organisedDuty);
+		} else {
+			console.log(rawRosterData);
+			return { data, rawRosterData };
+		}
 	}
-	return data;
+	console.log(data);
+	return { data, rawRosterData };
 };
 
-const keys = { isNotRostered: /\s*(\d+\/\d+)\s+(\w{3})\s+(LSC|NON AV)\s+(LS|PLN)/ };
+const keys = {
+	isNotRostered: /\s*(\d+\/\d+)\s+(\w{3})\s+(LSC|NON AV)\s+(LS|PLN|ASN)/,
+	isRostered: {
+		main: /\s*(\d+\/\d+)\s+(\w{3})\s+([A-Z0-9]{4,6})+\s+((\w{1,5}\/*)*)\s+((\d{4})\s+(\d{4}))\s+(\d{1,2}:\d{1,2})\s+(\w{3})*\s+(\w*)\s+(\w{2,6})\s+(\d{1,2}:\d{1,2})/,
+		hra_span: /\s*(\d+\/\d+)\s+(\w{3})\s+(HRA SPAN)\s+((\d{4})\s+(\d{4}))\s+(\d{1,2}:\d{1,2})\s+(\w{3})*\s+(\w*)\s+(\w{2,6})\s+(\d{1,2}:\d{1,2})/,
+		isOvernight: /\s*(\d+\/\d+)\s+(\w{3})\s+((\w{1,5}\/*)*)\s+((\d{4})\s+(\d{4}))\s+(\d{1,2}:\d{1,2})\s+(\w{3})*\s+(\w*)\s+()(\d{1,2}:\d{1,2})/,
+	},
+};
+
+// hra span
